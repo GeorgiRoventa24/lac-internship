313697c8e03ee833ce50eef35182c46f06ed94bc
GeorgiRoventa24 <ana.roventa@student.upt.ro>
2018-04-30 22:45:44 +0300

    changes before fetch

--- src/MlApiComp/Controllers/ExperimentsController.cs
+++ src/MlApiComp/Controllers/ExperimentsController.cs
@@ -1,23 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MlApiComp.Models;
 
 namespace MlApiComp.Controllers
 {
-    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Route("api/Experiments")]
     public class ExperimentsController : Controller
     {
-        private static int counter;
-
-        [HttpPost]
-        public IActionResult CreateExperiment(Experiment experiment)
-        {
-            experiment.Id = counter++;
-
-            return Ok(experiment);
-        }
     }
 }
